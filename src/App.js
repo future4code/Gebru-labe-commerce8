e60782fc90f7875de9d899534f58cb77596de2b4
LyonEscalli <lyonescalli@gmail.com>
2022-02-15 19:25:01 -0300

    Atualizei o arquivo card.js e App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import styled from 'styled-components';
 import {ComponenteFiltro} from './Components/filtro.js';
 import Card from './Components/Card';
 
 class App extends React.Component{
-  
+
+  state =  {
+    camisetas: [
+        {
+            id: 1,
+            name: "Camiseta de Astronauta",
+            value: 25.0,
+            imageUrl: "https://www.usecamisetas.com/media/product/dae/camiseta-infantil-astronauta-espaco-a9b.jpg"
+        },
+        {
+            id: 2,
+            name: "Camiseta de Planetas",
+            value: 50.0,
+            imageUrl: "https://www.casasbahia-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=1620164052"
+        },
+        {
+            id: 3,
+            name: "Camiseta de Foguete",
+            value: 75.0,
+            imageUrl: "https://images.tcdn.com.br/img/img_prod/742661/camiseta_infantil_masculina_preta_foguete_2165_1_20201022092501.jpg"
+        },
+        {
+            id: 4,
+            name: "Camiseta de Robô",
+            value: 100.0,
+            imageUrl: "https://www.camisetas4fun.com.br/media/product/190/camiseta-android-robo-r2-d2-3f5.jpg"
+        },
+    ]
+}
+
   render(){
+    const meusProdutos = this.state.camisetas.map((produto) => {
+      return <Card
+      produto={produto}
+      AdicionaCarrinho={this.props.AdicionarCarrinho}
+      />
+    })
     return (
       <div>
         <ComponenteFiltro/>
-        <Card />
+        {meusProdutos}
       </div>
     )
   }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -23,48 +23,18 @@ const ProdutoImagem = styled.img`
 
 export default class Card extends React.Component {
 
-    state =  {
-        camisetas: [
-            {
-                id: 1,
-                name: "Camiseta de Astronauta",
-                value: 25.0,
-                imageUrl: "https://www.usecamisetas.com/media/product/dae/camiseta-infantil-astronauta-espaco-a9b.jpg"
-            },
-            {
-                id: 2,
-                name: "Camiseta de Planetas",
-                value: 50.0,
-                imageUrl: "https://www.casasbahia-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=1620164052"
-            },
-            {
-                id: 3,
-                name: "Camiseta de Foguete",
-                value: 75.0,
-                imageUrl: "https://images.tcdn.com.br/img/img_prod/742661/camiseta_infantil_masculina_preta_foguete_2165_1_20201022092501.jpg"
-            },
-            {
-                id: 4,
-                name: "Camiseta de Robô",
-                value: 100.0,
-                imageUrl: "https://www.camisetas4fun.com.br/media/product/190/camiseta-android-robo-r2-d2-3f5.jpg"
-            },
-        ]
-    }
 
     render() {
     
-        const meusProdutos = this.state.camisetas.map((item) => <ContainerPrincipal>
-        <ProdutoImagem src={item.imageUrl} alt='Imagem da camiseta'></ProdutoImagem>
-        <p><b>{item.name}</b></p>
-        <p>Valor do produto: R${item.value},00</p>
-        <button onClick={this.AdicionaCarrinho}>Adicionar no carrinho</button>
-        </ContainerPrincipal>)
+        const produto = this.props.produto
 
       return (
-        <div>
-          {meusProdutos}
-      </div>
+        <ContainerPrincipal>
+            <ProdutoImagem src={produto.imageUrl}></ProdutoImagem>
+            <p><b>{produto.name}</b></p>
+            <p>Valor do produto: R${produto.value},00</p>
+            <button onClick={() => this.props.AdicionaCarrinho}>Adicionar no carrinho</button>
+        </ContainerPrincipal>
       )
     }
   }
