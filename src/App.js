7ffd5935f8f56fe63f364336757d3e458aaed9ca
LyonEscalli <lyonescalli@gmail.com>
2022-02-15 12:33:52 -0300

    Adicionei o card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import {ComponenteFiltro} from './Components/filtro.js';
+import Card from './Components/Card';
 
 class App extends React.Component{
   
@@ -8,6 +9,7 @@ class App extends React.Component{
     return (
       <div>
         <ComponenteFiltro/>
+        <Card />
       </div>
     )
   }
