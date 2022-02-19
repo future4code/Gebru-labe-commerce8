9887c498290b73c9f52d1d6015e2874d9fa49510
Coimbrax <lucascoimbralago@gmail.com>
2022-02-18 21:06:19 -0300

    Entrega Parcial Sexta

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// Import dos componentes
+// Import dos componentes //
 import React from 'react';
 import {ComponenteFiltro} from './Components/PastaFilters/filtro';
 import listaProd from './Lista/listaDeProdutos.json';
